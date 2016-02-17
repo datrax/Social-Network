bcdb660611d0647da766bbc5dc46e0790c636224
Denis Datrax <datrax@mail.ru>
2016-02-17 17:39:25 +0200

    Exception handling in BLL

--- SocialNetwork/UserStore.BLL/Services/PageService.cs
+++ SocialNetwork/UserStore.BLL/Services/PageService.cs
@@ -23,30 +23,44 @@ namespace UserStore.BLL.Services
 
         public UserDTO GetUserByID(string id)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
-            var mapper = config.CreateMapper();
-            return mapper.Map<UserDTO>(Database.Users.Find(a => a.Id == id).FirstOrDefault());
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
+                var mapper = config.CreateMapper();
+                return mapper.Map<UserDTO>(Database.Users.Find(a => a.Id == id).FirstOrDefault());
+            }
+            catch
+            {
+                return null;
+            }
         }
 
         public UserDTO GetUserByLogin(string login)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
-            var mapper = config.CreateMapper();
-            var t = Database.Users.Find(a => a.Login == login).ToList();
-            if (t.Count == 0)
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
+                var mapper = config.CreateMapper();
+                return mapper.Map<UserDTO>(Database.Users.Find(a => a.Login == login).FirstOrDefault());
+            }
+            catch
+            {
                 return null;
-            return mapper.Map<UserDTO>(Database.Users.Find(a => a.Login == login).First());
+            }
         }
 
         public AvatarDTO GetAvatar(string login)
         {
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<Photo, AvatarDTO>());
-            var mapper = config.CreateMapper();
-            var t = Database.Avatars.Find(a => a.UserId == login).ToList();
-            if (t.Count == 0)
+            try
+            {
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<Photo, AvatarDTO>());
+                var mapper = config.CreateMapper();
+                return mapper.Map<AvatarDTO>(Database.Avatars.Find(a => a.UserId == login).FirstOrDefault());
+            }
+            catch
+            {
                 return null;
-            return mapper.Map<AvatarDTO>(Database.Avatars.Find(a => a.UserId == login).First());
-
+            }
         }
 
         public bool SetAvatar(AvatarDTO avatar)
@@ -75,175 +89,235 @@ namespace UserStore.BLL.Services
 
         public string ChangeUserInfo(UserDTO user)
         {
-            if (string.IsNullOrEmpty(user.Name))
-                return "Please enter name";
+            try
+            {
+                if (string.IsNullOrEmpty(user.Name))
+                    return "Please enter name";
 
-            if (string.IsNullOrEmpty(user.Surname))
-                return "Please enter surname";
-            if (GetUserByLogin(user.Login) != null && GetUserByLogin(user.Login).Id != user.Id)
-                return "This URL is busy";
-            if (!(user.Login.All(c => Char.IsLetterOrDigit(c) || c == '_') && user.Login.Any(char.IsLetter)))
-                return "URL can contain only letters, numbers and '_' and must have at least one letter!";
+                if (string.IsNullOrEmpty(user.Surname))
+                    return "Please enter surname";
+                if (GetUserByLogin(user.Login) != null && GetUserByLogin(user.Login).Id != user.Id)
+                    return "This URL is busy";
+                if (!(user.Login.All(c => Char.IsLetterOrDigit(c) || c == '_') && user.Login.Any(char.IsLetter)))
+                    return "URL can contain only letters, numbers and '_' and must have at least one letter!";
 
 
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<UserDTO, ClientProfile>());
-            var mapper = config.CreateMapper();
-            Database.Users.Update(mapper.Map<ClientProfile>(user));
-            Database.Save();
-            return null;
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<UserDTO, ClientProfile>());
+                var mapper = config.CreateMapper();
+                Database.Users.Update(mapper.Map<ClientProfile>(user));
+                Database.Save();
+                return null;
+            }
+            catch (Exception)
+            {
+                return "Internal server error";
+            }
         }
 
         public IEnumerable<UserDTO> FindUsers(string input)
         {
-            var t = input.IndexOf(" ");
-            string name = "", surname = "";
-            if (t > 0)
+            try
             {
-                name = input.Substring(0, t);
-                surname = input.Substring(t + 1);
-            }
-            else
-            {
-                name = input;
-            }
-            if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(surname))
-                return new List<UserDTO>();
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
-            var mapper = config.CreateMapper();
-            if (name == "")
-            {
-                return
-                   mapper.Map<List<UserDTO>>(
-                               Database.Users.Find(a => a.Surname.ToLower().Contains(surname.ToLower())));
-            }
-            else
-            {
-                if (surname == "")
+                var t = input.IndexOf(" ");
+                string name = "", surname = "";
+                if (t > 0)
                 {
-                    return mapper.Map<List<UserDTO>>(Database.Users.Find(a => a.Name.ToLower().Contains(name.ToLower())));
+                    name = input.Substring(0, t);
+                    surname = input.Substring(t + 1);
                 }
-                return
-                    mapper.Map<List<UserDTO>>(
-                        Database.Users.Find(
-                            a =>
-                                a.Name.ToLower().Contains(name.ToLower()) &&
-                                a.Surname.ToLower().Contains(surname.ToLower())).ToList());
+                else
+                {
+                    name = input;
+                }
+                if (string.IsNullOrEmpty(name) && string.IsNullOrEmpty(surname))
+                    return new List<UserDTO>();
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
+                var mapper = config.CreateMapper();
+                if (name == "")
+                {
+                    return
+                       mapper.Map<List<UserDTO>>(
+                                   Database.Users.Find(a => a.Surname.ToLower().Contains(surname.ToLower())));
+                }
+                else
+                {
+                    if (surname == "")
+                    {
+                        return mapper.Map<List<UserDTO>>(Database.Users.Find(a => a.Name.ToLower().Contains(name.ToLower())));
+                    }
+                    return
+                        mapper.Map<List<UserDTO>>(
+                            Database.Users.Find(
+                                a =>
+                                    a.Name.ToLower().Contains(name.ToLower()) &&
+                                    a.Surname.ToLower().Contains(surname.ToLower())).ToList());
+                }
             }
+            catch (Exception)
+            {
+                return null;
+            }
+           
         }
 
         public IEnumerable<PostDTO> GetPosts(string authorizeId, string urlId)
         {
-            var user = Database.Users.Find(a => a.Id == urlId).First();
-            var posts = user.OnWallPosts.ToList();
-            List<PostDTO> wallPosts = new List<PostDTO>();
-            foreach (var post in posts)
+            try
             {
-                wallPosts.Add(new PostDTO()
+                var user = Database.Users.Find(a => a.Id == urlId).First();
+                var posts = user.OnWallPosts.ToList();
+                List<PostDTO> wallPosts = new List<PostDTO>();
+                foreach (var post in posts)
                 {
-                    Id = post.Id,
-                    DateTime = post.DateTime,
-                    Name = post.UserFrom.Name,
-                    Surname = post.UserFrom.Surname,
-                    Text = post.Text,
-                    CanDelete = authorizeId == post.UserFromId || authorizeId == post.UserToId,
-                    UserId = post.UserFromId,
-                    LikesCount = post.LikesUserPost.Count(),
-                    Login = post.UserFrom.Login,
-                    HasPhoto = post.HasPhoto
-                });
+                    wallPosts.Add(new PostDTO()
+                    {
+                        Id = post.Id,
+                        DateTime = post.DateTime,
+                        Name = post.UserFrom.Name,
+                        Surname = post.UserFrom.Surname,
+                        Text = post.Text,
+                        CanDelete = authorizeId == post.UserFromId || authorizeId == post.UserToId,
+                        UserId = post.UserFromId,
+                        LikesCount = post.LikesUserPost.Count(),
+                        Login = post.UserFrom.Login,
+                        HasPhoto = post.HasPhoto
+                    });
+                }
+                return wallPosts.OrderByDescending(a => a.DateTime);
             }
-            return wallPosts.OrderByDescending(a => a.DateTime);
+            catch (Exception)
+            {
+
+                return null;
+            }
+   
         }
         public bool AddPost(string authorizeId, string urlId, string text, byte[] image)
         {
-            if (image == null && string.IsNullOrEmpty(text))
+            try
+            {
+                if (image == null && string.IsNullOrEmpty(text))
+                {
+                    return false;
+                }
+                Post post = new Post()
+                {
+                    DateTime = DateTime.Now,
+                    Text = text,
+                    UserFromId = authorizeId,
+                    UserToId = urlId,
+                    HasPhoto = image != null
+                };
+                Database.Posts.Create(post);
+                if (image != null)
+                {
+                    var headerImage = new AvatarDTO()
+                    {
+                        Avatar = image,
+                        UserId = post.Id.ToString()
+                    };
+                    var config = new MapperConfiguration(cfg => cfg.CreateMap<AvatarDTO, Photo>());
+                    var mapper = config.CreateMapper();
+                    Database.Avatars.Create(mapper.Map<Photo>(headerImage));
+                }
+                Database.Save();
+                return true;
+            }
+            catch
             {
                 return false;
             }
-            Post post = new Post()
-            {
-                DateTime = DateTime.Now,
-                Text = text,
-                UserFromId = authorizeId,
-                UserToId = urlId,
-                HasPhoto = image != null
-            };
-            Database.Posts.Create(post);
-            if (image != null)
-            {
-                var headerImage = new AvatarDTO()
-                {
-                    Avatar = image,
-                    UserId = post.Id.ToString()
-                };
-                var config = new MapperConfiguration(cfg => cfg.CreateMap<AvatarDTO, Photo>());
-                var mapper = config.CreateMapper();
-                Database.Avatars.Create(mapper.Map<Photo>(headerImage));
-            }
-            Database.Save();
-            return true;
         }
         public bool DeletePost(int postId, string postWallOwnerId)
         {
-            var post = Database.Posts.Get(postId);
-            if (post.UserFromId != postWallOwnerId && post.UserToId != postWallOwnerId)
+            try
+            {
+                var post = Database.Posts.Get(postId);
+                if (post.UserFromId != postWallOwnerId && post.UserToId != postWallOwnerId)
+                {
+                    return false;
+                }
+                var t = post.LikesUserPost.ToList();
+                foreach (var like in t)
+                {
+                    Database.Likes.Delete(like.Id);
+                }
+                var photos = Database.Avatars.Find(a => a.UserId == post.Id.ToString());
+                foreach (var photo in photos)
+                {
+                    Database.Avatars.Delete(Int32.Parse(photo.UserId));
+                }
+
+
+                Database.Posts.Delete(postId);
+
+                Database.Save();
+                return true;
+            }
+            catch
             {
                 return false;
             }
-            var t = post.LikesUserPost.ToList();
-            foreach (var like in t)
-            {
-                Database.Likes.Delete(like.Id);
-            }
-            var photos = Database.Avatars.Find(a => a.UserId == post.Id.ToString());
-            foreach (var photo in photos)
-            {
-                Database.Avatars.Delete(Int32.Parse(photo.UserId));
-            }
-
-
-            Database.Posts.Delete(postId);
-
-            Database.Save();
-            return true;
         }
         public bool LikePost(string authorizeId, int postId)
         {
-            var post = Database.Posts.Get(postId);
-            var t = post.LikesUserPost.Where(a => a.ClientProfileId == authorizeId).ToList();
+            try
+            {
+                var post = Database.Posts.Get(postId);
+                var t = post.LikesUserPost.Where(a => a.ClientProfileId == authorizeId).ToList();
 
-            if (t.Count == 0)
-            {
-                post.LikesUserPost.Add(new LikesUserPost()
+                if (t.Count == 0)
                 {
-                    PostId = postId,
-                    ClientProfileId = authorizeId
-                });
+                    post.LikesUserPost.Add(new LikesUserPost()
+                    {
+                        PostId = postId,
+                        ClientProfileId = authorizeId
+                    });
+                }
+                else
+                {
+                    Database.Likes.Delete(t.First().Id);
+                }
+                Database.Save();
+                return true;
             }
-            else
+            catch
             {
-                Database.Likes.Delete(t.First().Id);
+                return false;
             }
-            Database.Save();
-            return true;
         }
 
         public IEnumerable<UserDTO> GetLikeUserList(int postId)
         {
-            List<ClientProfile> users = new List<ClientProfile>();
-            var post = Database.Posts.Get(postId);
-            foreach (var t in post.LikesUserPost)
+            try
             {
-                users.Add(t.ClientProfile);
+                List<ClientProfile> users = new List<ClientProfile>();
+                var post = Database.Posts.Get(postId);
+                foreach (var t in post.LikesUserPost)
+                {
+                    users.Add(t.ClientProfile);
+                }
+                var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
+                var mapper = config.CreateMapper();
+                return mapper.Map<List<UserDTO>>(users);
             }
-            var config = new MapperConfiguration(cfg => cfg.CreateMap<ClientProfile, UserDTO>());
-            var mapper = config.CreateMapper();
-            return mapper.Map<List<UserDTO>>(users);
+            catch
+            {
+                return null;
+            }
         }
 
         public string GetPostWallOwnerById(int postId)
         {
-            return Database.Posts.Get(postId).UserToId;
+            try
+            {
+                return Database.Posts.Get(postId).UserToId;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+           
         }
     }
 }
